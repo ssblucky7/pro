4da8275944d1739d7d1139ed62fdd1067018c2a7
ssblucky7 <SURESHKUMARMAHATOSCI2021@Prime.edu.np>
2025-01-17 19:24:03 +0545

    change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portfolio
+        S.S.B. Lucky 7
       </a>
       <div className={styles.menu}>
         <img
